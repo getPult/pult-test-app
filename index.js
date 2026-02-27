85abb2f5aa0a2ff1e6ba480a541ffdb6ad3228b8
Paul <paularnacfr@gmail.com>
2026-02-27 15:33:42 -0500

    feat: v5 cache test 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
   res.writeHead(200, {"Content-Type": "text/html"});
-  res.end("<h1>Pult Test App v4</h1><p>Build cache + rate limit test</p>");
+  res.end("<h1>Pult Test App v5</h1><p>Build cache test #2</p>");
 });
 server.listen(process.env.PORT || 3000);
