07405a5b37bf08e3bd17155d65a25074f426ffce
Paul <paularnacfr@gmail.com>
2026-02-27 14:58:45 -0500

    feat: preview v2 with DNS

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1>Preview Build</h1><p>This is a PR preview deployment!</p><p>Branch: feature/test-preview</p>");
+  res.end("<h1>Preview Build v2</h1><p>PR preview with DNS!</p><p>Branch: feature/test-preview</p>");
 });
 server.listen(process.env.PORT || 3000, () => {
   console.log("listening on port " + (process.env.PORT || 3000));
