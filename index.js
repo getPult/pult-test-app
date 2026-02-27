26572511f852eed7adeb1a58fbb6421e432e32fa
Paul <paularnacfr@gmail.com>
2026-02-27 15:35:47 -0500

    feat: v7 registry cache build 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
   res.writeHead(200, {"Content-Type": "text/html"});
-  res.end("<h1>Pult Test App v6</h1><p>Registry cache build 1</p>");
+  res.end("<h1>Pult Test App v7</h1><p>Registry cache build 2 (should be faster)</p>");
 });
 server.listen(process.env.PORT || 3000);
